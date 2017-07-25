f88d30eff6e0da7be7c18147b4a6cd0a00c46c0e
paola <bernodiana@gmail.com>
2017-07-25 12:30:38 -0500

    objetos

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+function Pasajeros (name,last_name,dni){
+	this.name=name;
+	this.last_name=last_name;
+	this.dni=dni;
+
+	this.listar= function(){};
+	this.reservar=function(){};
+	this.cancelar=function(){};
+	this.buscar=function(){};
+	this.seleccionar=function(){};
+}
+function Asientos(number){
+	this.numeroAsientos=new array(number);
+	
+
+    
+
+}
